7f023d5c88bf4f80d55ab7f17f930cf2fd4790de
jefflike <2609749420@qq.com>
2018-03-26 15:09:54 +0800

    day2java中的变量与数据类型及类型转换

---
+++ day002/datatype.java
@@ -0,0 +1,29 @@
+class java_type{
+	public static void main(String[] args){
+		//byte类型,整型默认值都为0
+		byte myByte = 100;
+		System.out.println(myByte);
+		//short类型
+		short myShort = 233;
+		System.out.println(myShort);
+		//int类型
+		int myInt = 23333;
+		System.out.println(myInt);
+		//long类型，默认值0L
+		long myLong = 12345678910111213L;
+		System.out.println(myLong);
+		//float类型，默认值是 0.0f
+		float myFloat = 234.5f;
+		System.out.println(myFloat);
+		//double类型，默认值是 0.0d
+		double myDouble = 3.14;
+		System.out.println(myDouble);
+		//boolean类型，默认值是 false
+		boolean myBoolean = true;
+		System.out.println(myBoolean);
+		//char类型
+		char myChar= 'J';
+		System.out.println(myChar);
+	}
+
+}
---
+++ day002/variable.java
@@ -0,0 +1,10 @@
+public class variable{
+	public static void main(String[] args){
+		int myInt = 233;
+		double myDouble = 3.14;
+
+		System.out.println(myInt);
+		System.out.println(myInt + myDouble);
+	}
+
+}
